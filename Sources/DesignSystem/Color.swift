2e340b646bab1af810881e886178e89d8e05b194
Jorge Mendoza <jorgevmendoza@gmail.com>
2022-06-28 01:40:15 +0200

    new image

--- Sources/DesignSystem/Color.swift
+++ Sources/DesignSystem/Color.swift
@@ -13,6 +13,7 @@ public extension Color {
         public static let success = Color(red: 0, green: 0.48235294222831726, blue: 1, opacity: 1)
         public static let neutral4 = Color(red: 1, green: 0.686274528503418, blue: 0.45490196347236633, opacity: 1)
         public static let neutral3 = Color(red: 0.18431372940540314, green: 0.800000011920929, blue: 0.8039215803146362, opacity: 1)
+        public static let neutral6 = Color(red: 0.9607843160629272, green: 0.9607843160629272, blue: 0.9607843160629272, opacity: 1)
     }
 }
 
